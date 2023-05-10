c9ca84732f5aab2f1a2297bf023e3d0e2e77177e
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-05-10 15:30:10 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -8,9 +8,6 @@ Write-Host "Starting SQL Server instance: $SQL_INSTANCE_NAME" -ForegroundColor C
 Start-Service "MSSQL`$$SQL_INSTANCE_NAME"
 Write-Host 'Service started...'
 
-Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL16.SQL2022\MSSQLServer\SuperSocketNetLib\Tcp\IPAll
-Computer\HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Microsoft SQL Server\MSSQL16.SQL2022\MSSQLServer\SuperSocketNetLib\Tcp\IP6
-
 #cmd /c sqlcmd -S localhost -U SA -P Password12! -Q "select @@VERSION"
 Write-Host 'Testing connectivity with named instance over named pipes'
 $env:connection_string_with_instance = "Server=(local)\$SQL_INSTANCE_NAME;Database=master;User ID=sa;Password=Password12!"
