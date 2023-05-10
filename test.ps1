e7e6dd309846b010521e41fafa207a0d7ba11e6f
Owen McDonnell <7119543+OwenMcDonnell@users.noreply.github.com>
2023-05-10 15:01:54 -0700

    Update test.ps1

--- test.ps1
+++ test.ps1
@@ -1,16 +1,12 @@
 $ErrorActionPreference = "Stop"
-Install-Module sqlserver -Force -AllowClobber -AcceptLicense
+
 $SQL_INSTANCE_NAME = $env:SQL_INSTANCE_NAME
 
 Write-Host "Starting SQL Server instance: $SQL_INSTANCE_NAME" -ForegroundColor Cyan
 Start-Service "MSSQL`$$SQL_INSTANCE_NAME"
 Write-Host 'Service started...'
-Write-Host "$env:computername\$SQL_INSTANCE_NAME"
-Get-InstalledModule -Name "sqlserver"
-Get-SqlInstance -ServerInstance "APPVEYOR-VM\SQL2022" | Set-SqlNetworkConfiguration -Protocol TCP -Port 1433 -ForceServiceRestart -AcceptSelfSignedCertificate
 
-#Get-Service | Where {$_.status -eq 'running' -and $_.DisplayName -match "sql server*"}
-cmd /c sqlcmd -S localhost -U SA -P Password12! -Q "select @@VERSION"
+#cmd /c sqlcmd -S localhost -U SA -P Password12! -Q "select @@VERSION"
 Write-Host 'Testing connectivity with named instance over named pipes'
 $env:connection_string_with_instance = "Server=(local)\$SQL_INSTANCE_NAME;Database=master;User ID=sa;Password=Password12!"
 $conn = New-Object System.Data.SqlClient.SqlConnection
